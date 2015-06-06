589ceb0873e5d06e1ec88abed5909e27c20dc4ef
drBolsen <al_mamistov@mail.ru>
2015-06-06 08:45:00 +0300

    Initial. OpenFile Dialog.

---
+++ Replacer/FileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Replacer
+    {
+    class FileProcessor
+        {
+        private IEnumerable<FileInfo> Files;
+
+        public void SetFiles(IEnumerable<string> paths)
+            {
+            Files = paths.Select(p => new FileInfo(p));
+            }
+        }
+    }
---
+++ Replacer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Replacer
+    {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+        {
+
+        private Microsoft.Win32.OpenFileDialog OpenFileDlg = new Microsoft.Win32.OpenFileDialog()
+        {
+            Filter = "MS Word documents (*.doc;*.docx)|*.doc;*.docx",
+            Multiselect = true,
+            CheckFileExists = true
+        };
+
+        private FileProcessor FileProcessor = new FileProcessor();
+
+        public MainWindow()
+            {
+            InitializeComponent();
+            }
+
+        private void OpenFiles(object sender, RoutedEventArgs e)
+            {
+            OpenFileDlg.ShowDialog(this);
+            FileProcessor.SetFiles(OpenFileDlg.FileNames);
+            }
+        }
+    }
